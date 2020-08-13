172216ce4330b730f24af631f3a1061b753bdb05
Dracoh <hectorsantossv@yahoo.com>
2020-08-12 23:02:29 -0600

    Segundo commit
    MAs color al diseño

--- medical/build/generated-sources/ap-source-output/entities/AppointmentStatus_.java
+++ medical/build/generated-sources/ap-source-output/entities/AppointmentStatus_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-08-12T22:05:12")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-08-12T22:24:58")
 @StaticMetamodel(AppointmentStatus.class)
 public class AppointmentStatus_ { 
 
